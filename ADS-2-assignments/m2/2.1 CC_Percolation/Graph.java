ffdd3389323bffd05a151319c228fb972b4486ac
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-10-31 17:37:31 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m2/2.1 CC_Percolation/Graph.java
+++ ADS-2-assignments/m2/2.1 CC_Percolation/Graph.java
@@ -1,10 +1,14 @@
-import java.util.NoSuchElementException;
-
+//import java.util.NoSuchElementException;
+/**
+ * Class for graph.
+ */
 public class Graph {
+    /**
+     * New Line.
+     */
     private static final String NEWLINE = System.getProperty("line.separator");
-
-    private final int V;
-    private int E;
+    private final int ver;
+    private int edg;
     private Bag<Integer>[] adj;
 
     /**
@@ -14,12 +18,15 @@ public class Graph {
      * @param  V number of vertices
      * @throws IllegalArgumentException if {@code V < 0}
      */
-    public Graph(int V) {
-        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
+    public Graph(int ver) {
+        if (ver < 0) { 
+            throw new IllegalArgumentException
+            ("Number of vertices must be nonnegative");
+        }
+        this.ver = ver;
+        this.edg = 0;
+        adj = (Bag<Integer>[]) new Bag[ver];
+        for (int v = 0; v < ver; v++) {
             adj[v] = new Bag<Integer>();
         }
     }
@@ -30,7 +37,7 @@ public class Graph {
      * @return the number of vertices in this graph
      */
     public int V() {
-        return V;
+        return ver;
     }
 
     /**
@@ -39,13 +46,13 @@ public class Graph {
      * @return the number of edges in this graph
      */
     public int E() {
-        return E;
+        return edg;
     }
 
     // throw an IllegalArgumentException unless {@code 0 <= v < V}
     private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+        if (v < 0 || v >= ver)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (ver - 1));
     }
 
     /**
@@ -58,7 +65,7 @@ public class Graph {
     public void addEdge(int v, int w) {
         validateVertex(v);
         validateVertex(w);
-        E++;
+        edg++;
         adj[v].add(w);
         adj[w].add(v);
     }
@@ -113,8 +120,8 @@ public class Graph {
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
+        s.append(ver + " vertices, " + edg + " edges " + NEWLINE);
+        for (int v = 0; v < ver; v++) {
             s.append(v + ": ");
             for (int w : adj[v]) {
                 s.append(w + " ");
