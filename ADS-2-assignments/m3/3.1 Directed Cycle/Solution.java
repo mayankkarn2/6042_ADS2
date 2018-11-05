77559e4add8d5b5b27703688b98b3be586b9159d
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-11-05 17:25:10 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m3/3.1 Directed Cycle/Solution.java
+++ ADS-2-assignments/m3/3.1 Directed Cycle/Solution.java
@@ -2,35 +2,35 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//Nothing happens here.
-	}
-	/**
-	 * The main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int v = Integer.parseInt(sc.nextLine());
-		int e = Integer.parseInt(sc.nextLine());
-		Digraph d = new Digraph(v);
-		while (e > 0) {
-			String[] tokens = sc.nextLine().split(" ");
-			int a = Integer.parseInt(tokens[0]);
-			int b = Integer.parseInt(tokens[1]);
-			d.addEdge(a, b);
-			e--;
-		}
-		DirectedCycle dc = new DirectedCycle(d);
-		if (dc.hasCycle()) {
-			System.out.println("Cycle exists.");
-		} else {
-			System.out.println("Cycle doesn't exists.");
-		}
-	}
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Nothing happens here.
+    }
+    /**
+     * The main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int v = Integer.parseInt(sc.nextLine());
+        int e = Integer.parseInt(sc.nextLine());
+        Digraph d = new Digraph(v);
+        while (e > 0) {
+            String[] tokens = sc.nextLine().split(" ");
+            int a = Integer.parseInt(tokens[0]);
+            int b = Integer.parseInt(tokens[1]);
+            d.addEdge(a, b);
+            e--;
+        }
+        DirectedCycle dc = new DirectedCycle(d);
+        if (dc.hasCycle()) {
+            System.out.println("Cycle exists.");
+        } else {
+            System.out.println("Cycle doesn't exists.");
+        }
+    }
 }
