4b1d2f6f26637d2e90f02e989c62aadf41743670
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-11-05 17:52:44 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m3/3.2 Bipartite/Solution.java
+++ ADS-2-assignments/m3/3.2 Bipartite/Solution.java
@@ -2,32 +2,33 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
-		//Private Solution.
-	}
-	/**
-	 * The main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int v = Integer.parseInt(s.nextLine());
-		int e = Integer.parseInt(s.nextLine());
-		Graph g = new Graph(v);
-		for (int i = 0; i < e; i++) {
-			String[] tokens = s.nextLine().split(" ");
-			g.addEdge(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]));
-		}
-		DirectedCycle dc = new DirectedCycle(g);
-		if (dc.isBipartite()) {
-			System.out.println("Graph is bipartite");
-		} else {
-			System.out.println("Graph is not a bipartite");
-		}
-	}
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Private Solution.
+    }
+    /**
+     * The main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int v = Integer.parseInt(s.nextLine());
+        int e = Integer.parseInt(s.nextLine());
+        Graph g = new Graph(v);
+        for (int i = 0; i < e; i++) {
+            String[] tokens = s.nextLine().split(" ");
+            g.addEdge(Integer.parseInt(tokens[0]),
+                Integer.parseInt(tokens[1]));
+        }
+        DirectedCycle dc = new DirectedCycle(g);
+        if (dc.isBipartite()) {
+            System.out.println("Graph is bipartite");
+        } else {
+            System.out.println("Graph is not a bipartite");
+        }
+    }
 }
