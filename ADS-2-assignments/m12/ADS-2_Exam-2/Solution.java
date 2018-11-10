729f810d7b11e5c9f26ac63834554ebbe6dbe257
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-11-10 12:31:07 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
-
-	public static void main(String[] args) {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//Nothing happens here.
+	}
+	/**
+	 * The main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		// Self loops are not allowed...
 		// Parallel Edges are allowed...
 		// Take the Graph input here...
@@ -13,17 +26,21 @@ public class Solution {
 		EdgeWeightedDigraph g = new EdgeWeightedDigraph(cities);
 		for(int i = 0; i < roadLines; i++) {
 			String[] tokens = sc.nextLine().split(" ");
-			gr.addEdge(new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2])));
-			g.addEdge(new DirectedEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2])));
-			g.addEdge(new DirectedEdge(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[0]), Integer.parseInt(tokens[2])));
-
+			gr.addEdge(new Edge(Integer.parseInt(tokens[0]),
+				Integer.parseInt(tokens[1]),
+				Integer.parseInt(tokens[2])));
+			g.addEdge(new DirectedEdge(Integer.parseInt(tokens[0]),
+				Integer.parseInt(tokens[1]),
+				Integer.parseInt(tokens[2])));
+			g.addEdge(new DirectedEdge(Integer.parseInt(tokens[1]),
+				Integer.parseInt(tokens[0]),
+				Integer.parseInt(tokens[2])));
 		}
 		String caseToGo = sc.nextLine();
 		switch (caseToGo) {
 		case "Graph":
 			System.out.println(gr);
 			break;
-
 		case "DirectedPaths":
 			// Handle the case of DirectedPaths, where two integers are given.
 			// First is the source and second is the destination.
