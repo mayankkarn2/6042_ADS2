1dbe27efde21234ab2828efaeb0c8423a31425f9
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-11-14 16:48:45 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m13/Radix Sorts/LSD.java
+++ ADS-2-assignments/m13/Radix Sorts/LSD.java
@@ -18,28 +18,32 @@ public class LSD {
      */
     public static void sort(final String[] a, final int w) {
         int n = a.length;
-        int radix = 256;   // extend ASCII alphabet size
+        final int radix = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
 
-        for (int d = w-1; d >= 0; d--) {
+        for (int d = w - 1; d >= 0; d--) {
             // sort by key-indexed counting on dth character
 
             // compute frequency counts
-            int[] count = new int[radix+1];
-            for (int i = 0; i < n; i++)
+            int[] count = new int[radix + 1];
+            for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
+            }
 
             // compute cumulates
-            for (int r = 0; r < radix; r++)
-                count[r+1] += count[r];
+            for (int r = 0; r < radix; r++) {
+                count[r + 1] += count[r];
+            }
 
             // move data
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 aux[count[a[i].charAt(d)]++] = a[i];
+            }
 
             // copy back
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 a[i] = aux[i];
+            }
         }
     }
 }
