17d9f3734002e1c5bc70797b2e8f80e060935d1f
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-11-17 21:24:52 +0530

    "ads2_m4_a1_1: 13/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -48,7 +48,7 @@ class Solution {
                     String[] tokens = sc.nextLine().split(" ");
                     try {
                         wrdnet.sap(tokens[0], tokens[1]);
-                        System.out.println("distance ="
+                        System.out.println("distance = "
                             + wrdnet.distance(tokens[0], tokens[1])
                             + ", ancestor = " + wrdnet.sap(tokens[0], tokens[1]));
                     } catch (Exception e) {
